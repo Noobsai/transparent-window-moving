877279ddae54837adfbef9d3bf560389393455db
Noobsai <ewgdogg@gmail.com>
2020-10-07 01:07:31 +0500

    Removed unnecessary code

--- src/extension.js
+++ src/extension.js
@@ -5,7 +5,7 @@ const GLib = imports.gi.GLib;
 const ExtensionUtils = imports.misc.extensionUtils;
 
 let _settings = null;
-let _WindowState;
+let _window_opacity;
 
 let _on_window_grab_begin, _on_window_grab_end;
 let _on_move_changed, _on_resize_changed;
@@ -51,12 +51,8 @@ function is_grab_operation_allowed(grab_op) {
   return _allowed_grab_operations.indexOf(grab_op) > -1; 
 }
 
-function set_opacity(window_surface, target_opacity, state, on_complete, check_if_completed) {
-  let thread = Date.now();
-  state.thread = thread;
-
+function set_opacity(window_surface, target_opacity, on_complete) {
   let complete_func = function() { 
-    state.thread = 0;
     if (on_complete) { 
       on_complete(); 
     }
@@ -72,31 +68,14 @@ function set_opacity(window_surface, target_opacity, state, on_complete, check_i
       mode: Clutter.AnimationMode.EASE_OUT_QUAD, 
       opacity: target_opacity,
       onComplete: complete_func
-  });
-    if (check_if_completed) {
-      // Repair opacity if the Tween was canceled
-      // Seems was fixed in 3.36
-      set_timeout(function() { 
-        if (state && state.thread == thread){
-          window_surface.opacity = target_opacity;
-          complete_func();
-        }
-      }, transition_time * 1000 + 100);
-    }
+    });
   }
-}
-
-function set_timeout(func, time){
-  GLib.timeout_add(GLib.PRIORITY_DEFAULT, time, function() {
-    func();
-    return false;
-  });
 }
 
 function get_window_surface(meta_window) {
   let window_actor = meta_window.get_compositor_private();
-  var childs = window_actor.get_children();
-  for (var i = 0; i < childs.length; i++) {
+  let childs = window_actor.get_children();
+  for (let i = 0; i < childs.length; i++) {
     if (childs[i].constructor.name.indexOf('MetaSurfaceActor') > -1) {
       return childs[i];
     }
@@ -112,14 +91,12 @@ function window_grab_begin(meta_display, meta_screen, meta_window, meta_grab_op,
 
   let window_surface = get_window_surface(meta_window);
   let pid = meta_window.get_pid();
-  let state = _WindowState[pid];
-  if (!state) {
-    state = { thread: -1, original_opacity: window_surface.opacity }
-    _WindowState[pid] = state;
+  if (!_window_opacity[pid]) {
+    _window_opacity[pid] = window_surface.opacity; 
   }
 
   let opacity_value = _settings.get_int('window-opacity');
-  set_opacity(window_surface, opacity_value, state);
+  set_opacity(window_surface, opacity_value);
 }
 
 function window_grab_end(meta_display, meta_screen, meta_window, meta_grab_op, gpointer) {
@@ -129,15 +106,14 @@ function window_grab_end(meta_display, meta_screen, meta_window, meta_grab_op, g
 
   let window_surface = get_window_surface(meta_window);
   let pid = meta_window.get_pid();
-  let state = _WindowState[pid];
 
-  set_opacity(window_surface, state.original_opacity, state, function() { delete _WindowState[pid]; }, true);
+  set_opacity(window_surface, _window_opacity[pid], function() { delete _window_opacity[pid]; });
 }
 
 function enable() {
   _settings = ExtensionUtils.getSettings();
   init_grab_operations();
-  _WindowState = {};
+  _window_opacity = {};
   _on_window_grab_begin = global.display.connect('grab-op-begin', window_grab_begin);
   _on_window_grab_end = global.display.connect('grab-op-end', window_grab_end);
   _on_move_changed = _settings.connect('changed::transparent-on-moving', init_grab_operations);
@@ -150,7 +126,7 @@ function disable() {
   _settings.disconnect(_on_move_changed);
   _settings.disconnect(_on_resize_changed);
 
-  _WindowState = {};
+  _window_opacity = {};
   _settings.run_dispose();
 }
 
